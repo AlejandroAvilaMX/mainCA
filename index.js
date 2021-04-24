b5e6f9523d9e20b9444c7bc8ab11731bd824c060
AlejandroAvilaMX <alejandroavila0494@hotmail.com>
2021-04-24 01:48:26 +0000

    routes.js was created

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const http = require('http'),
 logger = require('morgan'),
-cors = requite('cors'),
+cors = require('cors'),
 express = require('express'),
 bodyParser = require('body-parser');
 
 var app = express();
 var port = 8000;
 
-http.createServer((req, res)=>{
-    res.write("Hello World"); 
-    res.end(); 
- }).listen(8000);
+app.use(bodyParser.json());
+app.use(logger('tiny'));
+
+app.get ('/hello', (req, res) => {
+    res.write("Test");
+    res.write(users.join('\n'));
+});
+
+ app.listen(port, function(err){
+    console.log('Listening on port: ' + port);
+});
