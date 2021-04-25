8fdb57bea1e188a4b9daf6cb6f18fc98bbc07538
AlejandroAvilaMX <alejandroavila0494@hotmail.com>
2021-04-25 21:35:52 +0000

    find one

--- catalog-controller.js
+++ catalog-controller.js
@@ -19,3 +19,31 @@ exports.getItems = function(req, res) {
     res.json(catalog);
   }); 
 };
+
+exports.getItem = function(req, res) {
+  Catalog.findOne({_id: req.params.id}, function (err, catalog) {
+    if (err) {
+      res.status(400).json(err); 
+    } 
+    res.json(catalog);
+  }); 
+};
+
+/*
+exports.updateUser = function(req, res) {
+  User.findByIdAndUpdate({_id: req.params.id}, req.body, {new: true}, function (err, users) {
+    if (err) {
+      res.status(400).json(err); 
+    } 
+    res.json(users);
+  }); 
+};
+
+exports.deleteUser = function(req, res) {
+  User.findByIdAndRemove({_id: req.params.id}, function (err, users) {
+    if (err) {
+      res.status(400).json(err); 
+    } 
+    res.json(users);
+  }); 
+};*/
--- routes.js
+++ routes.js
@@ -5,5 +5,8 @@ itemCtrl = require('./catalog-controller');
 
 router.post('/items', itemCtrl.createItem);
 router.get('/items', itemCtrl.getItems);
+router.get('/items/:id', itemCtrl.getItem);
+//router.put('/items/:id', itemCtrl.updateItem);
+//router.delete('/items/:id', itemCtrl.deleteItem);
 
 module.exports = router;
