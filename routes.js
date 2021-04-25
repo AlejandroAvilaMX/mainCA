d9b4d00716f42db0c3fa489a1c738345543adda8
AlejandroAvilaMX <alejandroavila0494@hotmail.com>
2021-04-25 23:53:00 +0000

    upload images

--- routes.js
+++ routes.js
@@ -3,7 +3,7 @@ router = express.Router();
 
 itemCtrl = require('./catalog-controller');
 
-//Routes that call all the methods to manipulate the information
+//Routes that call all the methods to manipulate the information of the catalog of items
 router.post('/items', itemCtrl.createItem);
 router.get('/items', itemCtrl.getItems);
 router.get('/items/:id', itemCtrl.getItem);
@@ -15,5 +15,11 @@ module.exports.UPLOAD_PATH = "uploads";     //This path will tell to multer wher
 var multer = require("multer");             //Include the module multer
 var upload = multer({ dest: module.exports.UPLOAD_PATH});           //Instance of the multer
 var imageCtrl = require('./image-controller');                      //Provide the functionality
+
+//Routes that call the methos to manipulate images
+router.post('/images', upload.single('image'), imageCtrl.uploadImage);
+router.get('/images', imageCtrl.getImages);
+router.get('/images/:id', imageCtrl.getImage);
+router.delete('/images/:id', imageCtrl.deleteImage);
 
 module.exports = router;        //Make it available to the other files
