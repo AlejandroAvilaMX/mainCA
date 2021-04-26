0324623211eb397dd387dca478692f488aa8f9a7
AlejandroAvilaMX <alejandroavila0494@hotmail.com>
2021-04-26 10:24:55 +0000

    .env

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 //Those are the modules that our app will need
-const http = require('http'),           //Provides the http functionality
-logger = require('morgan'),             //morgan is our logger
-cors = require('cors'),                 //This allow us to do the cross side request
-express = require('express'),           //
-bodyParser = require('body-parser'),
-mongoose = require('mongoose');         //Allow us to connect to oyr Database 
+const http = require('http'),               //Provides the http functionality
+logger = require('morgan'),                 //morgan is our logger
+cors = require('cors'),                     //This allow us to do the cross side request
+express = require('express'),               //Working with express
+bodyParser = require('body-parser'),        //Working with body Parser
+mongoose = require('mongoose');             //Allow us to connect to our Database 
+dotenv = require("dotenv");                 //Working with dotenv
 
-var app = express();                                            //This is the module that allow us set and star the server 
-var port = 8000;                                                //This is the port
+var app = express();                        //This is the module that allow us set and star the server 
+var port = 8000;                            //This is the port
 
 app.use(bodyParser.json());                 //To be able to wokr and parse the json data
 app.use(logger('tiny'));
 app.use(require('./routes'));               //To work with our routes independenly 
+dotenv.config();                            //Specify tha we are going to work with our .env file
 
 app.get ('/hello', (req, res) => {
     res.write("Test");
@@ -35,7 +37,7 @@ app.listen(port, function(err){
 
 //Connect mongoose with MondgoDB
 //const dbURI = "mongodb://localhost/test";       //Conect to MongoDB locally
-const dbURI = "mongodb+srv://pass1234:pass1234@cluster0.4o0vp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";       //Conect to MongoDB web
+const dbURI = process.env.DB_URL;       //Conect to MongoDB Atlas
 
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
         .then((result) => console.log('connected to db'))
